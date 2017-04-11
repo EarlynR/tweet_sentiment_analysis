6ee0422516f894af6857f34bc3bbb39b5858b30a
Adams LaBorde <alaborde@umbel.com>
2017-04-10 20:30:51 -0500

    reimplement table.insert

--- twitter/tweepy_scraper.py
+++ twitter/tweepy_scraper.py
@@ -1,5 +1,8 @@
-import tweepy
-import sqlalchemy
+from tweepy import StreamListener, Stream
+
+import settings
+from settings import api
+from app import db
 
 # TODO: Create local db & settings module
 # db = dataset.connect(settings.connection_string)
@@ -11,7 +14,6 @@ class StreamListener(tweepy.StreamListener):
         if status.retweeted:
             return
             
-        #Included are all of the fields from the twitter api that I think are relevant.     
         user_name= status.user.screen_name
         users_loc = status.user.location
         tweets_coords = status.coordinates
@@ -25,30 +27,25 @@ class StreamListener(tweepy.StreamListener):
 
         #Getting ready to put my inputs into a table
         # TODO: Implement db
-        # table = db[settings.table_name]
+        table = db[settings.table_name]
 
-        # table.insert(dict(
-        #     user_name=user_name
-        #     user_location=users_loc,
-        #     tweet_coordinates=tweets_coords,
-        #     text=text,
-        #     symbols_in_text = symbols_in_text,
-        #     hastags_in_text = hastags_in_text,
-        #     tweet_created_at = tweet_created_at,
-        #     lead_score = sent.lead_score,
-        # ))
+        table.insert(dict(
+            user_name=user_name
+            user_location=users_loc,
+            tweet_coordinates=tweets_coords,
+            text=text,
+            symbols_in_text = symbols_in_text,
+            hastags_in_text = hastags_in_text,
+            tweet_created_at = tweet_created_at,
+            # lead_score = sent.lead_score,
+        ))
 
     def on_error(self, status_code):
         if status_code == 420:
             #returning False in on_data disconnects the stream
             return False
 
-#Opening a connection to the Twitter api with proper keys. 
-# auth = tweepy.OAuthHandler(settings.consumer_key, settings.consumer_secret)
-# auth.set_access_token(settings.access_token, settings.access_token_secret)
-api = tweepy.API(auth)
-
 #Start streaming the data.
 stream_listener = StreamListener()
-stream = tweepy.Stream(auth=api.auth, listener=stream_listener)
+stream = Stream(auth=api.auth, listener=stream_listener)
 stream.filter(languages=['en'], filter_level=['none'])
