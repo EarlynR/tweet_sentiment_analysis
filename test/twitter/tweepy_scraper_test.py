ec93f9911bbfbdf970ec28dd27f9dd5a1ea7642c
Adams LaBorde <alaborde@umbel.com>
2017-04-15 13:33:28 -0500

    tests initial commit

---
+++ test/twitter/tweepy_scraper_test.py
@@ -0,0 +1,33 @@
+import os
+from unittest import TestCase
+
+from app import app, db
+from settings import BASE_DIR
+from twitter import tweepy_scraper
+from app.models import TwitterUser, Tweet
+
+
+class TweepyScraperTest(TestCase):
+    def setUp(self):
+        app.config['TESTING'] = True
+        app.config['WTF_CSRF_ENABLED'] = False
+        app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(BASE_DIR, 'test.db')
+        self.app = app.test_client()
+        db.create_all()
+
+    def tearDown(self):
+        db.session.remove()
+        db.drop_all()
+
+    def test_add_user(self):
+        test_user = TwitterUser(user_name="@TestUser", location="test_coords")
+
+        self.assertIsNone(db.session.query(test_user))
+
+        db.session.add(test_user)
+        db.session.commit()
+
+        print db.session.query(test_user)
+        assert False
+
+
